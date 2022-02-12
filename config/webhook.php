02ca44caec82967af2e92d10b7125660473015d0
Aluminum <91447283+AluminumFF@users.noreply.github.com>
2022-02-12 17:06:58 +0300

    Create webhook.php

---
+++ config/webhook.php
@@ -0,0 +1,6 @@
+<?php
+$webhook = ""; // Change to your discord webhook url if you want to use it
+
+$hookNewLevels = true; // Send message when users upload a level
+$hookRates = true; // Send message when moderator rates a level
+?>
