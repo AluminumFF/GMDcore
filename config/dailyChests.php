9227f5dc63265604268c2d8e37488f71912dad2b
Aluminum <91447283+AluminumFF@users.noreply.github.com>
2022-02-12 18:55:18 +0300

    Update dailyChests.php

--- config/dailyChests.php
+++ config/dailyChests.php
@@ -6,15 +6,15 @@
 /*
 	REWARDS
 */
-//SMALL CHEST
-$chest1minOrbs = 200;
+//Маленький сундук
+$chest1minOrbs = 200; 
 $chest1maxOrbs = 400;
 $chest1minDiamonds = 2;
 $chest1maxDiamonds = 10;
 $chest1items = [1, 2, 3, 4, 5, 6];
 $chest1minKeys = 1;
 $chest1maxKeys = 6;
-//BIG CHEST
+//Большой сундук
 $chest2minOrbs = 2000;
 $chest2maxOrbs = 4000;
 $chest2minDiamonds = 20;
@@ -22,7 +22,7 @@ $chest2maxDiamonds = 100;
 $chest2items = [1, 2, 3, 4, 5, 6];
 $chest2minKeys = 1;
 $chest2maxKeys = 6;
-//REWARD TIMES (in seconds)
+//Лут время (в Секундах)
 $chest1wait = 3600;
 $chest2wait = 14400;
 ?>
