89ebe5e8d41b8cbe306703d8720cafe2f8e02931
Aluminium <91447283+AluminumFF@users.noreply.github.com>
2022-02-13 11:36:08 +0300

    Update likeGJItem.php

--- incl/misc/likeGJItem.php
+++ incl/misc/likeGJItem.php
@@ -3,15 +3,18 @@ chdir(dirname(__FILE__));
 include "../lib/connection.php";
 require_once "../lib/exploitPatch.php";
 require_once "../lib/mainLib.php";
+require_once "../lib/GJPCheck.php";
 $gs = new mainLib();
 
-if(!isset($_POST['itemID']))
+if(!isset($_POST['itemID']) || !isset($_POST['accountID']) || !isset($_POST['gjp']))
 	exit(-1);
 
 $type = isset($_POST['type']) ? $_POST['type'] : 1;
 $itemID = ExploitPatch::remove($_POST['itemID']);
 $isLike = isset($_POST['like']) ? $_POST['like'] : 1;
 $ip = $gs->getIP();
+$GJPCheck = new GJPCheck();
+if($GJPCheck->check($_POST['gjp'], $_POST['accountID']) != 1) exit('<iframe width="560" height="315" src="https://www.youtube.com/embed/dQw4w9WgXcQ" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>'); 
 
 $query = $db->prepare("SELECT count(*) FROM actions_likes WHERE itemID=:itemID AND type=:type AND ip=INET6_ATON(:ip)");
 $query->execute([':type' => $type, ':itemID' => $itemID, ':ip' => $ip]);
